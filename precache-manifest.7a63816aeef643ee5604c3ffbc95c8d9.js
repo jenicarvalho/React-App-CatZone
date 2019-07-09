5bfb410728449fcdbecab8f1a366103c73266bb8
Jeniffer Carvalho <jenicarvalho@live.com>
2019-07-09 18:46:21 -0300

    Updates

---
+++ precache-manifest.7a63816aeef643ee5604c3ffbc95c8d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bc0e884105c7caa6af92434c363808",
+    "url": "/React-App-CatZone/index.html"
+  },
+  {
+    "revision": "e09a89a8e1b2514d2bc8",
+    "url": "/React-App-CatZone/static/js/2.cc866176.chunk.js"
+  },
+  {
+    "revision": "1f58ed3c288f9fd72089",
+    "url": "/React-App-CatZone/static/js/main.96a57b2e.chunk.js"
+  },
+  {
+    "revision": "ba17776f9545c3230f78",
+    "url": "/React-App-CatZone/static/js/runtime~main.a2080bbe.js"
+  }
+]);
